0174da994ad81a044c7f8a22c116c1d696160eb0
tatsuru koike <t-koike@marsflag.com>
2024-03-24 16:17:34 +0900

    feat: add trim function

--- src/components/Trimming.vue
+++ src/components/Trimming.vue
@@ -14,11 +14,6 @@ const store = mainStore()
 const sliderCanvas = ref<HTMLCanvasElement | CanvasRenderingContext2D | null>(null)
 
 /****************************************
- * data
- ****************************************/
-const progressBarPosition = ref<number>(0)
-
-/****************************************
  * props
  ****************************************/
 const props = defineProps({
@@ -44,65 +39,104 @@ const canvasHeight = computed(() => {
 })
 
 /****************************************
- * emit
- ****************************************/
-const emit = defineEmits(['trim-start', 'trim-end'])
-
-/****************************************
  * handlers
  ****************************************/
 let isDragging = ref<boolean>(false)
+let isDraggingLeft = ref<boolean>(false)
+let isDraggingRight = ref<boolean>(false)
+const trimmingSliderWrapper = ref<HTMLDivElement | null>(null)
 const trimmingSlider = ref<HTMLDivElement | null>(null)
 const progressBar = ref<HTMLDivElement | null>(null)
 const cursorType = ref<string>('pointer')
-
-const progressBarPositionCalc = (e: MouseEvent) => {
-  return (progressBarPosition.value = e.clientX - progressBar.value!.offsetWidth / 2 - 112)
-}
-
-const handleMouseDown = (e: MouseEvent) => {
-  progressBarPosition.value = progressBarPositionCalc(e)
-  cursorType.value = 'grabbing'
-  isDragging.value = true
-}
-
-const handleMouseUp = (e: MouseEvent) => {
-  cursorType.value = 'pointer'
-  isDragging.value = false
-
-  store.currentTime =
-    store.videoDuration * (progressBarPosition.value / trimmingSlider.value!.clientWidth)
-}
-
-const handleMouseDbClick = (e: MouseEvent) => {
-  if (!store.playFlag) {
-    progressBarPosition.value = progressBarPositionCalc(e)
-    store.currentTime =
-      store.videoDuration * (progressBarPosition.value / trimmingSlider.value!.clientWidth)
-  }
-}
+const trimmingSliderBackgroundColor = ref<string>('#cccccc')
+const progressBarPosition = ref<number>(22)
+const trimmingSliderWidth = ref<number>(0)
 
 const handleMouseMove = (e: MouseEvent) => {
-  if (isDragging.value) {
-    if (progressBarPositionCalc(e) < 0) {
-      progressBarPosition.value = 0
-      return
-    }
-    progressBarPosition.value = progressBarPositionCalc(e)
+  if (isDraggingRight.value) {
+    trimmingSliderWidthCalcForRightTrim(e)
+  }
+  if (isDraggingLeft.value) {
+    trimmingSliderWidthCalcForLeftTrim(e)
   }
 }
 
-const handleMouseLeave = () => {
-  isDragging.value = false
+const handleMouseLeave = (e: MouseEvent) => {
+  isDraggingRight.value = false
+  isDraggingLeft.value = false
+  trimmingSliderBackgroundColor.value = '#cccccc'
+  cursorType.value = 'pointer'
+}
+
+const progressBarPositionCalc = (e: MouseEvent) => {
+  progressBarPosition.value = e.clientX - progressBar.value!.offsetWidth / 2 - 112
+}
+
+// for trimming slider width
+const trimmingSliderWidthCalcForRightTrim = (e: MouseEvent) => {
+  if (e.clientX <= trimmingSliderWrapper.value!.offsetWidth + 110) {
+    trimmingSliderWidth.value = e.clientX - 110
+  } else {
+    trimmingSliderWidth.value = trimmingSliderWrapper.value!.offsetWidth
+  }
+}
+
+const trimmingSliderWidthCalcForLeftTrim = (e: MouseEvent) => {
+  // when left trim slider, slider left position should be changed and width should be changed
+  if (e.clientX > 120) {
+    trimmingSliderWidth.value =
+      trimmingSliderWidth.value + (trimmingSlider.value!.offsetLeft - e.clientX + 120)
+    console.log(trimmingSliderWidth.value)
+    trimmingSlider.value!.style.left = `${e.clientX - 120}px`
+  } else if (0 <= e.clientX) {
+    trimmingSliderWidth.value = trimmingSliderWrapper.value!.offsetWidth
+  }
+}
+
+// right trim slider
+const handleMouseHoverForTrimmingSliderRight = (e: MouseEvent) => {
+  if (!isDraggingRight.value) {
+    cursorType.value = 'grab'
+  }
+}
+
+const handleMouseDownForTrimmingSliderRight = (e: MouseEvent) => {
+  isDraggingRight.value = true
+  trimmingSliderBackgroundColor.value = '#ffc800'
+  cursorType.value = 'grabbing'
+}
+
+const handleMouseUpForTrimmingSliderRight = (e: MouseEvent) => {
+  isDraggingRight.value = false
+  trimmingSliderBackgroundColor.value = '#cccccc'
+  cursorType.value = 'grab'
+}
+
+const handleMouseLeaveForTrimmingSliderRight = (e: MouseEvent) => {
   // cursorType.value = 'pointer'
 }
 
-const handleMouseClick = (e: MouseEvent) => {
-  isDragging.value = !isDragging.value
+// for left trim slider
+const handleMouseHoverForTrimmingSliderLeft = (e: MouseEvent) => {
+  if (!isDraggingLeft.value) {
+    cursorType.value = 'grab'
+  }
 }
 
-const handleMouseOver = () => {
-  // cursorType.value = 'grab'
+const handleMouseDownForTrimmingSliderLeft = (e: MouseEvent) => {
+  isDraggingLeft.value = true
+  trimmingSliderBackgroundColor.value = '#ffc800'
+  cursorType.value = 'grabbing'
+}
+
+const handleMouseUpForTrimmingSliderLeft = (e: MouseEvent) => {
+  isDraggingLeft.value = false
+  trimmingSliderBackgroundColor.value = '#cccccc'
+  cursorType.value = 'grab'
+}
+
+const handleMouseLeaveForTrimmingSliderLeft = (e: MouseEvent) => {
+  // cursorType.value = 'pointer'
 }
 
 /****************************************
@@ -113,7 +147,9 @@ const handleMouseOver = () => {
 /****************************************
  * onMounted
  ****************************************/
-onMounted(() => {})
+onMounted(() => {
+  trimmingSliderWidth.value = trimmingSliderWrapper.value!.offsetWidth
+})
 
 /****************************************
  * watch
@@ -132,69 +168,150 @@ watch(
 
 <template>
   <div
-    class="trimmingSlider"
-    @dblclick="handleMouseDbClick"
+    class="trimmingSliderWrapper"
+    ref="trimmingSliderWrapper"
     @mousemove="handleMouseMove"
     @mouseleave="handleMouseLeave"
-    ref="trimmingSlider"
-    :style="{ cursor: cursorType }"
   >
-    <canvas ref="sliderCanvas" style="user-select: none" />
+    <!-- ↓trim slider↓ -->
     <div
+      class="trimmingSlider"
+      ref="trimmingSlider"
+      :style="{
+        width: `${trimmingSliderWidth}px`,
+        cursor: cursorType
+      }"
+    >
+      <div
+        class="leftArrowWrapper"
+        :style="{ backgroundColor: trimmingSliderBackgroundColor }"
+        @mouseover="handleMouseHoverForTrimmingSliderLeft"
+        @mousedown="handleMouseDownForTrimmingSliderLeft"
+        @mouseup="handleMouseUpForTrimmingSliderLeft"
+        @mouseleave="handleMouseLeaveForTrimmingSliderLeft"
+      >
+        <div class="leftArrow">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+            <path fill="currentColor" d="m14 17l-5-5l5-5z" />
+          </svg>
+        </div>
+      </div>
+      <div
+        class="rightArrowWrapper"
+        :style="{ backgroundColor: trimmingSliderBackgroundColor }"
+        @mouseover="handleMouseHoverForTrimmingSliderRight"
+        @mousedown="handleMouseDownForTrimmingSliderRight"
+        @mouseup="handleMouseUpForTrimmingSliderRight"
+        @mouseleave="handleMouseLeaveForTrimmingSliderRight"
+      >
+        <div class="rightArrow">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M10 17V7l5 5z" />
+          </svg>
+        </div>
+      </div>
+    </div>
+    <canvas ref="sliderCanvas" style="user-select: none" />
+    <!-- ↑trim slider↑ -->
+
+    <!-- <div
       class="progressBar"
-      @mousedown="handleMouseDown"
-      @mouseover="handleMouseOver"
-      @mouseup="handleMouseUp"
       :style="{ left: `${progressBarPosition}px`, cursor: cursorType }"
       ref="progressBar"
-    ></div>
+    ></div> -->
   </div>
 </template>
 
 <style scoped lang="scss">
-.trimmingSlider {
+.trimmingSliderWrapper {
   position: relative;
   width: 100%;
   height: 100%;
-  cursor: pointer;
+  border-radius: 10px;
+  background: #cccccc;
+  padding: 10px 0;
+
+  canvas {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: calc(100% - 44px);
+    height: calc(100% - 20px);
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    background: black;
+  }
 }
 
-canvas {
+.trimmingSlider {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
   width: 100%;
   height: 100%;
-  border: 5px solid #ffc800;
-  border-radius: 8px;
-  padding: 10px;
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-  background: black;
-  margin-bottom: 4px;
-}
+  padding: 10px 22px;
+  border-radius: 10px;
+  background-color: transparent;
 
-.second-indicator {
-  width: 4px;
-  height: 4px;
-  border-radius: 50%;
-  &--bold {
-    background: #6e7787;
+  .rightArrowWrapper {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 22px;
+    height: 100%;
+    background-color: #cccccc;
+    border-radius: 0 10px 10px 0;
+    .rightArrow {
+      position: absolute;
+      top: 50%;
+      right: -3px;
+      transform: translateY(-50%);
+      width: 30px;
+      height: 30px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      svg {
+        width: 100%;
+        height: 100%;
+      }
+    }
   }
-  &--light {
-    background: #afbbca;
+
+  .leftArrowWrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 22px;
+    height: 100%;
+    background-color: #c3c3c3;
+    border-radius: 10px 0 0 10px;
+    .leftArrow {
+      position: absolute;
+      top: 50%;
+      left: -3px;
+      transform: translateY(-50%);
+      width: 30px;
+      height: 30px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      svg {
+        width: 100%;
+        height: 100%;
+      }
+    }
   }
 }
 
 .progressBar {
   position: absolute;
   bottom: 0;
-  background: #6e7787;
+  background: #fff;
   width: 5px;
   height: 100%;
-
-  &:hover {
-    background: #ffc800;
-  }
-
-  &:active {
-    background: #ffc800;
-  }
 }
 </style>
