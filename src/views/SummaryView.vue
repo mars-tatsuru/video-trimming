9434292b0b03729011671f64945c9eb241082001
tatsuru koike <t-koike@marsflag.com>
2024-03-29 17:38:18 +0900

    feat: add summary results page

--- src/views/SummaryView.vue
+++ src/views/SummaryView.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted, computed, watch } from 'vue'
-import VideoPlayer from '@/components/VideoPlayer.vue'
-import Trimming from '@/components/Trimming.vue'
-import exportButton from '@/components/ExportButton.vue'
-import ExportMp3Button from '@/components/ExportMp3Button.vue'
 import { mainStore } from '@/stores/main'
 import { useRouter } from 'vue-router'
-import { cloneDeep, isEqual } from 'lodash-es'
-import { on } from 'events'
 
 /****************************************
  * store and router
@@ -18,128 +12,42 @@ const router = useRouter()
 /****************************************
  * data
  ****************************************/
-const beforeForm = ref<any>({})
-const form = ref<any>({}) // SettingSchema is a type definition
-const isEditing = computed(() => {
-  return !isEqual(beforeForm.value, form.value)
-})
-const summaryText = ref<string>('')
-/****************************************
- * beforeEach
- ****************************************/
-// Display an alert if the edited data has not been saved before navigating to another page.
-// router.beforeEach((to, from, next) => {
-//   if (isEditing.value) {
-//     const answer = window.confirm('編集中のデータが全て破棄されますが、よろしいですか？')
-//     if (answer) {
-//       resetForm()
-//       next()
-//     } else {
-//       next(false)
-//     }
-//   } else {
-//     next()
-//   }
-// })
 
-function init() {
-  // Save the current time data in the store.
-  form.value = cloneDeep(0)
-  // Save he current time before editing for the purpose of comparing it with the data after editing in the form.
-  beforeForm.value = cloneDeep(store.videoSummaryText)
-}
-
-const resetForm = () => {
-  form.value = beforeForm.value
-}
-
-// watch(
-//   () => store.videoSummaryText,
-//   () => {
-//     summaryText.value = store.videoSummaryText
-//   }
-// )
-
+let newsData = ref() // will later be filled with the data
 onMounted(() => {
-  init()
+  // json parse
+  let req = new XMLHttpRequest()
+  req.open('GET', 'http://localhost:5173/newsData.json', false)
+  req.send(null)
+  if (req.status == 200) newsData.value = JSON.parse(req.responseText)
 })
 </script>
 
 <template>
   <div class="main">
-    <div class="summaryInput">
-      <p>動画全体文字起こし</p>
-      <textarea v-if="store.videoSummaryText" v-model="store.videoSummaryText" />
-      <p v-else>fetch error</p>
+    <div class="summaryWrapper">
+      <h1>要約結果画面</h1>
+      <div v-for="{ whole, title, summary, tags } in newsData" class="summaryItem">
+        <div v-if="whole" class="summaryItemContents">
+          <h2>ページ全体要約</h2>
+          <textarea v-text="summary" />
+          <div class="tags">
+            <p v-for="(tag, index) in tags">
+              {{ tag }}
+            </p>
+          </div>
+        </div>
+        <div v-else class="summaryItemContents">
+          <h2>{{ title }}</h2>
+          <textarea v-text="summary" />
+          <div class="tags">
+            <p v-for="(tag, index) in tags">
+              {{ tag }}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-    <!-- <div v-if="!store.videoSummaryText" class="loading">
-      <span class="loadingSvg">
-        <svg
-          class="waitingDownloadIcon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="1em"
-          height="1em"
-          viewBox="0 0 24 24"
-        >
-          <circle cx="12" cy="3.5" r="1.5" fill="currentColor" opacity="0">
-            <animateTransform
-              attributeName="transform"
-              calcMode="discrete"
-              dur="2.4s"
-              repeatCount="indefinite"
-              type="rotate"
-              values="0 12 12;90 12 12;180 12 12;270 12 12"
-            />
-            <animate
-              attributeName="opacity"
-              dur="0.6s"
-              keyTimes="0;0.5;1"
-              repeatCount="indefinite"
-              values="1;1;0"
-            />
-          </circle>
-          <circle cx="12" cy="3.5" r="1.5" fill="currentColor" opacity="0">
-            <animateTransform
-              attributeName="transform"
-              begin="0.2s"
-              calcMode="discrete"
-              dur="2.4s"
-              repeatCount="indefinite"
-              type="rotate"
-              values="30 12 12;120 12 12;210 12 12;300 12 12"
-            />
-            <animate
-              attributeName="opacity"
-              begin="0.2s"
-              dur="0.6s"
-              keyTimes="0;0.5;1"
-              repeatCount="indefinite"
-              values="1;1;0"
-            />
-          </circle>
-          <circle cx="12" cy="3.5" r="1.5" fill="currentColor" opacity="0">
-            <animateTransform
-              attributeName="transform"
-              begin="0.4s"
-              calcMode="discrete"
-              dur="2.4s"
-              repeatCount="indefinite"
-              type="rotate"
-              values="60 12 12;150 12 12;240 12 12;330 12 12"
-            />
-            <animate
-              attributeName="opacity"
-              begin="0.4s"
-              dur="0.6s"
-              keyTimes="0;0.5;1"
-              repeatCount="indefinite"
-              values="1;1;0"
-            />
-          </circle>
-        </svg>
-        要約文生成中
-      </span>
-    </div> -->
   </div>
 </template>
 
@@ -148,48 +56,54 @@ onMounted(() => {
   width: 100%;
   height: 100%;
 
-  .summaryInput {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    p {
-      font-size: 18px;
-      font-weight: bold;
-      margin-bottom: 10px;
-    }
-
-    textarea {
-      width: 80%;
-      height: 60vh;
-      padding: 20px;
-      font-size: 16px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-    }
+  h1 {
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 20px;
+    letter-spacing: 3px;
+    text-align: center;
   }
 
-  .loading {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
+  .summaryWrapper {
+    padding: 20px;
 
-    .loadingSvg {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 10px;
-      font-size: 18px;
-      color: #666;
-      font-weight: bold;
+    .summaryItem {
+      margin-top: 20px;
+      border: 1px solid #ccc;
+      padding: 20px;
+      border-radius: 10px;
 
-      svg {
-        width: 100px;
-        height: 100px;
+      .summaryItemContents {
+        h2 {
+          font-size: 20px;
+          font-weight: bold;
+          margin-bottom: 10px;
+        }
+
+        textarea {
+          width: 100%;
+          border: 1px solid #ccc;
+          max-height: 300px;
+          field-sizing: content;
+          margin-bottom: 15px;
+          font-size: 14px;
+          padding: 10px;
+          line-height: 1.5;
+          border-radius: 5px;
+        }
+
+        .tags {
+          display: flex;
+
+          p {
+            border-radius: 2px;
+            color: #fff;
+            padding: 8px 15px;
+            line-height: 1;
+            background-color: #6b77ff;
+            margin-right: 10px;
+          }
+        }
       }
     }
   }
